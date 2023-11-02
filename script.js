618f0a5388f8dcd4697e92372b0cccd42989494c
Daniel Gutkin <dngutkin@gmail.com>
2023-11-02 12:51:26 -0400

    Replaced work cards

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener(
 
 window.addEventListener(
     "load",
-    createWorkCardObserver
+    createWorkRightObserver
 );
 
 window.addEventListener(
@@ -15,12 +15,27 @@ window.addEventListener(
     animateHandPoint
 );
 
-
+// build intersection observers
 let options = {
     root: null,
     rootMargin: "0px",
     threshold: buildThresholdList()
 };
+
+function buildThresholdList() {
+
+    let thresholds = [];
+    let numSteps = 100;
+  
+    for (let i = 1.0; i <= numSteps; i++) {
+      let ratio = i / numSteps;
+      thresholds.push(ratio);
+    }
+  
+    thresholds.push(0);
+    return thresholds;
+
+}
 
 function createStoryRightObserver() {
 
@@ -32,39 +47,23 @@ function createStoryRightObserver() {
 
 }
 
-function createWorkCardObserver() {
+function createWorkRightObserver() {
 
-    workCardOne = document.getElementById("project-one");
-    workCardTwo = document.getElementById("project-two");
+    workRight = document.getElementById("work-right");
 
     let observer = new IntersectionObserver(workCardHandler, options);
 
-    observer.observe(workCardOne);
-    observer.observe(workCardTwo);
+    observer.observe(workRight);
 
 }
 
-function buildThresholdList() {
-
-    let thresholds = [];
-    let numSteps = 20;
-  
-    for (let i = 1.0; i <= numSteps; i++) {
-      let ratio = i / numSteps;
-      thresholds.push(ratio);
-    }
-  
-    thresholds.push(0);
-    return thresholds;
-
-  }
-
+// define intersection handlers
 function storyRightHandler(entries, observer) {
 
     entries.forEach( (entry) => {
         
+        // move to the right on scroll down (and left on scroll up)
         entry.target.style.opacity = entry.intersectionRatio;
-
         entry.target.style.transform = `translateX(${(1-entry.intersectionRatio) * 50}%)`
         
     });
@@ -73,16 +72,24 @@ function storyRightHandler(entries, observer) {
 
 function workCardHandler(entries, observer) {
 
+    const workCardArr = ["project-one", "project-two", "project-three"];
+
     entries.forEach( (entry) => {
         
-        entry.target.style.opacity = entry.intersectionRatio;
+        for (let workCard of workCardArr) {
 
-        entry.target.style.transform = `translateX(${(1-(entry.intersectionRatio)) * 30}%)`
+            // get a handle on work cards, fade and move to the right on scroll down
+            workCardElement = document.getElementById(workCard);
+            workCardElement.style.opacity = entry.intersectionRatio;
+            workCardElement.style.transform = `translateX(${(1-(entry.intersectionRatio)) * 30}%)`
+
+        }
         
     });
 
 }
 
+// define scroll to behaviour
 function scrollToHome() {
 
     // scroll to the top of the page
@@ -90,47 +97,48 @@ function scrollToHome() {
   
 }
   
-  function scrollToElement(elementId) {
-  
-    // fetch the element based on the id
-    const element = document.getElementById(elementId);
+function scrollToElement(elementId) {
+
+// fetch the element based on the id
+const element = document.getElementById(elementId);
+
+// get the top absolute position of the element
+const positionY = window.scrollY + element.getBoundingClientRect().top;
+
+// scroll to the position
+window.scrollTo(0, positionY);
+
+}
+
+// animate the hand point on the hero page
+function animateHandPoint() {
+
+const handPoint = document.getElementById("hand-point");
+let translateArr = [1, 2, 3, 4, 50]
+let direction = true;
+let counter = 0;
+
+// loop animation
+setInterval( () => {
     
-    // get the top absolute position of the element
-    const positionY = window.scrollY + element.getBoundingClientRect().top;
-  
-    // scroll to the position
-    window.scrollTo(0, positionY);
-  
-  }
+    // check direction
+    if (counter == translateArr.length - 1) {
+        direction = false;
+    } else if (counter == 0) {
+        direction = true;
+    }
 
-  function animateHandPoint() {
+    // increment or decrement counter based on direction
+    if (direction) {
+        counter++;
+    } else {
+        counter--;
+    }
 
-    const handPoint = document.getElementById("hand-point");
-    let translateArr = [1, 2, 3, 4, 50]
-    let direction = true;
-    let counter = 0;
+    let translate = translateArr[counter];
+    
+    handPoint.style.transform = `translateY(${translate}%)`;
+    
+}, 100);
 
-    // loop animation
-    setInterval( () => {
-        
-        // check direction
-        if (counter == translateArr.length - 1) {
-            direction = false;
-        } else if (counter == 0) {
-            direction = true;
-        }
-
-        // increment or decrement counter based on direction
-        if (direction) {
-            counter++;
-        } else {
-            counter--;
-        }
-
-        let translate = translateArr[counter];
-        
-        handPoint.style.transform = `translateY(${translate}%)`;
-        
-    }, 100);
-
-  }
+}
